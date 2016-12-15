2b9d53bf73d70c0adc8ce6250d0775964c34b980
Zachary King <kingzach77@gmail.com>
2016-12-15 09:01:24 -0600

    calc2 added support for multiplication and division

---
+++ calc2_2.py
@@ -0,0 +1,175 @@
+# Token types
+#
+# EOF (end-of-file) token is used to indicate that 
+# there is no more input left for lexical analysis
+INTEGER, PLUS, MINUS, MUL, DIV, EOF = 'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', 'EOF'
+
+
+class Token(object):
+    def __init__(self, type, value):
+        # Token type: INTEGER, PLUS, or EOF
+        self.type = type
+
+        # Token value: non-negative integer value, '+', '-', or None
+        self.value = value
+
+    def __str__(self):
+        """String representation of the class instance.
+        
+        Examples:
+            Token(INTEGER, 3)
+            Token(PLUS, '+')
+        """
+        return 'Token({type}, {value})'.format(
+            type=self.type,
+            value=repr(self.value)
+        )
+
+    def __repr__(self):
+        return self.__str__()
+
+
+
+class Interpreter(object):
+    def __init__(self, text):
+        # Client string input, e.g. "3 + 5", "12 - 5", etc.
+        self.text = text
+
+        # self.pos is an index into self.text
+        self.pos = 0
+
+        # Current token isntance
+        self.current_token = None
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception('Error parsing input')
+
+    def advance(self):
+        """Advance the 'pos' pointer and set the 'current_char' variable."""
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None # Indicates end of input
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        """Return a (multidigit) integer consumed from the input."""
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        """Lexical analyzer (also known as scanner or tokenizer)
+        
+        This method is responsible for breaking a sentence 
+        apart into tokens. One token at a time.
+        """
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+
+            if self.current_char == '*':
+                self.advance()
+                return Token(MUL, '*')
+
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIV, '/')
+
+            self.error()
+
+        return Token(EOF, None)
+
+    def eat(self, token_type):
+        # Compare the current token type with the passed token 
+        # type and if they match then "eat" the current token 
+        # and assign the next token to the self.current_token,
+        # otherwise raise an exception
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+
+    def expr(self):
+        """Parser / Interpreter
+        
+        expr -> INTEGER PLUS INTEGER
+        expr -> INTEGER MINUS INTEGER
+        """
+        # Set current token to the first token taken from the input
+        self.current_token = self.get_next_token()
+
+        # We expect the current token to be a single-digit integer
+        left = self.current_token
+        self.eat(INTEGER)
+
+        # We expect the current token to be a '+', '-', '*', or '/' token
+        op = self.current_token
+        if op.type == PLUS:
+            self.eat(PLUS)
+        elif op.type == MINUS:
+            self.eat(MINUS)
+        elif op.type == MUL:
+            self.eat(MUL)
+        else:
+            self.eat(DIV)
+
+        # We expect the current token to be a single-digit integer
+        right = self.current_token
+        self.eat(INTEGER)
+
+        # After the above call the self.current_token is set to EOF token
+
+        # At this point INTEGER PLUS INTEGER sequence of tokens 
+        # has been successfully found and the method can just 
+        # return the result of adding two integers, thus 
+        # effectively interpreting the client input
+        if op.type == PLUS:
+            result = left.value + right.value
+        elif op.type == MINUS:
+            result = left.value - right.value
+        elif op.type == MUL:
+            result = left.value * right.value
+        else:
+            if right.value == 0:
+                raise ZeroDivisionError("You can't divide by zero!! WTH!?")
+            else:
+                result = left.value / right.value
+        return result
+
+
+
+def main():
+    while True:
+        try:
+            text = input('calc> ')
+        except EOFError:
+            break
+        if not text:
+            continue
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
